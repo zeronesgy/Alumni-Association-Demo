840bd88fccccb9038900fa3ef3a4eb5575676eca
zeronesgy <246660764@qq.com>
2024-10-23 22:23:14 +0800

    1th41023
    引入viper
    从config文件中读取配置

--- common/database.go
+++ common/database.go
@@ -3,6 +3,7 @@ package common
 import (
 	"Alumni-Association-Demo/model"
 	"fmt"
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,15 +11,15 @@ import (
 var DB *gorm.DB
 
 func InitDB() *gorm.DB {
-	driverName := "mysql"
-	host := "localhost"
-	port := "3306"
-	database := "aad"
-	user := "root"
-	password := "root"
-	charset := "utf8mb4"
+	driverName := viper.GetString("datasource.driverName")
+	host := viper.GetString("datasource.host")
+	port := viper.GetString("datasource.port")
+	database := viper.GetString("datasource.database")
+	username := viper.GetString("datasource.username")
+	password := viper.GetString("datasource.password")
+	charset := viper.GetString("datasource.charset")
 	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=True&loc=Local",
-		user, password, host, port, database, charset)
+		username, password, host, port, database, charset)
 
 	db, err := gorm.Open(mysql.New(mysql.Config{DriverName: driverName, DSN: args}), &gorm.Config{})
 	if err != nil {
--- main.go
+++ main.go
@@ -2,13 +2,31 @@ package main
 
 import (
 	"Alumni-Association-Demo/common"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"os"
 )
 
 func main() {
+	InitConfig()
 	_ = common.InitDB()
 
 	r := gin.Default()
 	r = CollectRoute(r)
-	panic(r.Run(":8080"))
+	port := viper.GetString("server.port")
+	if port != "" {
+		panic(r.Run(":" + port))
+	}
+}
+
+func InitConfig() {
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("application")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(workDir + "/config/")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
 }
